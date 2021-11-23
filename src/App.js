fc81e5f8718c0af9a6589e659f53f71b129351fa
Nicolae Pop <nicupop729@gmail.com>
2021-11-23 18:37:08 -0300

    build the components in react and style it

--- src/App.js
+++ src/App.js
@@ -1,29 +1,21 @@
-import logo from './logo.svg';
+/* eslint-disable no-useless-constructor */
+/* eslint-disable react/prefer-stateless-function */
+import React, { Component } from 'react';
 import './App.css';
+import CalcComponets from './components/Calculator';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit
-          {' '}
-          <code>src/App.js</code>
-          {' '}
-          and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+  }
+
+  render() {
+    return (
+      <div>
+        <CalcComponets />
+      </div>
+    );
+  }
 }
 
 export default App;
