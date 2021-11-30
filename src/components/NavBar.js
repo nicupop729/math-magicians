0156f053bec60ecd3d89b6ba81575583cf34522b
Nicolae Pop <nicupop729@gmail.com>
2021-11-30 18:57:05 -0300

    change html tags in order to use semantic tags

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,26 +3,28 @@ import { Link } from 'react-router-dom';
 import './navbar.css';
 
 const NavBar = () => (
-  <div className="header">
+  <header className="header">
     <h1 className="nav-title">Math Magicians</h1>
-    <ul className="nav-list">
-      <li>
-        <Link to="/" className="active-link">
-          <p>Home</p>
-        </Link>
-      </li>
-      <li>
-        <Link to="/calc" className="active-link">
-          <p>Calculator</p>
-        </Link>
-      </li>
-      <li>
-        <Link to="/quote" className="active-link">
-          <p>Quote</p>
-        </Link>
-      </li>
-    </ul>
-  </div>
+    <nav>
+      <ul className="nav-list">
+        <li>
+          <Link to="/" className="active-link">
+            <p>Home</p>
+          </Link>
+        </li>
+        <li>
+          <Link to="/calc" className="active-link">
+            <p>Calculator</p>
+          </Link>
+        </li>
+        <li>
+          <Link to="/quote" className="active-link">
+            <p>Quote</p>
+          </Link>
+        </li>
+      </ul>
+    </nav>
+  </header>
 );
 
 export default NavBar;
